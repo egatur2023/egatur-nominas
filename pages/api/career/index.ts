7b946cb6f90212c2501b5998f4d9b010107749ac
egatur2023 <136015512+egatur2023@users.noreply.github.com>
2023-06-13 00:01:38 -0500

    Update index.ts

--- pages/api/career/index.ts
+++ pages/api/career/index.ts
@@ -3,10 +3,9 @@ import { prisma } from '../../../prisma/db'
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse ) {
     let careers = await prisma.career.findMany()
-    try {
+    if(careers) {
         return res.status(200).json(careers)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({message : 'No se pudo obtener las carreras.'})
     }
+    return res.status(401).json({message : ''})
+    
 }
