7cb6928c03a2b279ba7c11f7c7c1b83802ce930b
Diego Rolando Layme Valeriano <vandiego_pandax@hotmail.com>
2024-01-09 12:30:29 -0500

    habilitando admisiones sidebar

--- resources/routes.ts
+++ resources/routes.ts
@@ -25,11 +25,11 @@ export const itemsSidebarAdmin : AreaSidebar[] =
                     title : 'Solicitudes de cambio de nota',
                     path : '/app/admin/request',
                 },
-                // {
-                //     name : 'Admisiones',
-                //     title : 'Admisiones2',
-                //     path : '/app/admin/admission',
-                // },
+                {
+                    name : 'Admisiones',
+                    title : 'Admisiones2',
+                    path : '/app/admin/admission',
+                },
             ]
 
         },
