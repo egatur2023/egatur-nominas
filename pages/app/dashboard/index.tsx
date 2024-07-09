29a57b70903a5c10a4abfa7637980ec7cb4599df
egatur2023 <136015512+egatur2023@users.noreply.github.com>
2024-07-09 08:42:26 -0500

    prueba

--- pages/app/dashboard/index.tsx
+++ pages/app/dashboard/index.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function TableCourse() {
     return (
-        <Typography variant="h3"> Dashboard</Typography>
+        <Typography variant="h3"> Dashboard.</Typography>
     )
 }
 
