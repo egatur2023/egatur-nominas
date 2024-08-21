5f38592c21cfa035901ce277593968944438251c
egatur2023 <136015512+egatur2023@users.noreply.github.com>
2024-08-21 12:00:25 -0500

    Update insert.ts

--- resources/services/user/insert.ts
+++ resources/services/user/insert.ts
@@ -4,16 +4,22 @@ import { User } from "@prisma/client";
 // Función para crear un nuevo usuario
 export async function createUser(user: {
     email: string;
-    password: string;
+    password: string; // Asegúrate de encriptar la contraseña antes de almacenarla
     username: string;
     roleId: number;
 }): Promise<User> {
-    return await prisma.user.create({
-        data: {
-            email: user.email,
-            password: user.password,  // Si no usas encriptación, se almacena tal cual
-            username: user.username,
-            roleId: user.roleId,
-        },
-    });
+    try {
+        // Crear el usuario
+        return await prisma.user.create({
+            data: {
+                email: user.email,
+                password: user.password, // Considera encriptar la contraseña aquí
+                username: user.username,
+                roleId: user.roleId,
+            },
+        });
+    } catch (error) {
+        console.error("Error en la función createUser:", error);
+        throw new Error("Error al crear el usuario");
+    }
 }
